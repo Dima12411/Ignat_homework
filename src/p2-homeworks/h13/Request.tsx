e6a206371b378edb5533e036deb022d15697dbde
Dima12411 <fixer40delta@gmail.com>
2022-03-14 23:44:47 +0300

    create component Request

---
+++ src/p2-homeworks/h13/Request.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Request = () => {
+    return (
+        <div>
+            <button>x</button>
+            <input type="checkbox"/>
+        </div>
+    );
+};
+
+export default Request;
