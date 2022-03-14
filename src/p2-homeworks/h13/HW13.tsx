a5f771d15dd1571b760ff250f15b8047e6717085
Dima12411 <fixer40delta@gmail.com>
2022-03-15 00:19:03 +0300

    HW13 - complete

--- src/p2-homeworks/h13/HW13.tsx
+++ src/p2-homeworks/h13/HW13.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
+import {RequestAPI} from "./api/ApiForHW13";
 
 const HW13 = () => {
+    const [cheked, setCheked] = useState<boolean>(false)
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setCheked(e.currentTarget.checked)
+    }
+    const onClickHandler = () => {
+        RequestAPI.check(cheked)
+            .then((res) => {
+                return res.data
+            })
+            .catch ((error) => {
+                console.log({...error});
+                console.log(error.response ? error.response.data.errorText : error.message);
+            })
+    }
+
     return (
         <div>
-            <button>x</button>
-            <input type="checkbox"/>
+            <button onClick={onClickHandler}>Request</button>
+            <input onChange={onChangeHandler} type="checkbox"/>
         </div>
     );
 };
