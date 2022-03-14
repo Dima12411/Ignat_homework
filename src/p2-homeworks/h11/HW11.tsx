d1a158c7fef495171cfa9a6378df24b0248d4165
Dima12411 <fixer40delta@gmail.com>
2022-03-14 20:32:15 +0300

    HW11 - completed

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -3,28 +3,29 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+    const [value,setValue]=useState<Array<number>>([0,100])
+
+    const onChangeRange = (valueForRange: number) => {
+        setValue([valueForRange, value[1]])
+    }
+    const onChangeRange2 = (value: Array<number>) => {
+        debugger
+        setValue(value)
+    }
 
     return (
         <div>
             <hr/>
             homeworks 11
-
-            {/*should work (должно работать)*/}
             <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
+                <div>{value[0]}</div>
+                <SuperRange onChangeRange={onChangeRange} value={value[0]}/>
             </div>
 
             <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
+                <div>{value[0]}</div>
+                <SuperDoubleRange onChangeRange={onChangeRange2} value={value}/>
+                <div>{value[1]}</div>
             </div>
 
             <hr/>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,23 +1,62 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import Slider from "@material-ui/core/Slider";
+import {Box} from "@material-ui/core";
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
+    onChangeRange?: (value: Array<number>) => void
+    value?: Array<number>
 }
-
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
         onChangeRange, value,
-        // min, max, step, disable, ...
+
     }
 ) => {
-    // сделать самому, можно подключать библиотеки
+
+    const [value2, setValue2] = React.useState<Array<number>>([0, 100]);
+
+    useEffect(()=>{
+        if(value) {
+            setValue2(value)
+        }
+    },[value])
+
+    const minDistance = 10;
+
+    const handleChange2 = (
+        event: Event,
+        newValue: number | number[],
+        activeThumb: number,
+    ) => {
+        if (!Array.isArray(newValue)) {
+            return;
+        }
+        if (newValue[1] - newValue[0] < minDistance) {
+            if (activeThumb === 0) {
+                const clamped = Math.min(newValue[0], 100 - minDistance);
+                setValue2([clamped, clamped + minDistance]);
+            } else {
+                const clamped = Math.max(newValue[1], minDistance);
+                setValue2([clamped - minDistance, clamped]);
+            }
+        } else {
+            if (onChangeRange) {
+                onChangeRange(newValue)
+            }
+            setValue2(newValue as number[])
+        }
+    };
 
     return (
-        <>
-            DoubleRange
-        </>
+        <Box sx={{width: 300}}>
+            <Slider
+                getAriaLabel={() => 'Minimum distance shift'}
+                value={value2}
+                onChange={handleChange2}
+                valueLabelDisplay="auto"
+                disableSwap
+            />
+        </Box>
     )
 }
 
